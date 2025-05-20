d26b2efa893da60d0ac0b3441147c63187f487f0
Romklao Chainuwong <rk120879@gmail.com>
2025-05-20 15:19:57 -0700

    add ignoring eslint for building production

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
